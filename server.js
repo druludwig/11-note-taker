5f3a5213e3db01c9590f8a91e1d1b350e61a61dd
Dru Ludwig <druludwig@yahoo.com>
2021-07-23 19:24:21 -0700

    Live testing; still at midway point

--- server.js
+++ server.js
@@ -1,12 +1,25 @@
 const express = require('express');
 const app = express();
+const path = require('path')
 const data = require("./db/db.json")
 
 let PORT = 3001
 
+app.u
+app.use(express.json());
+
+app.get('/notes', (req,res)=>{
+    res.sendFile(path.join(__dirname, 'public/notes.html'));
+
+})
 
 app.get('/api/notes', (req,res)=>{
     res.json(data)
+})
+
+app.post('/api/notes', (req,res)=>{
+    data.push(req.body);
+    res.json(data) 
 })
 
 app.use(express.static('public'));
