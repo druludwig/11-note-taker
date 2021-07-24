ba06d9d3d645119511f04d13bcfc65a794816b22
Dru Ludwig <druludwig@yahoo.com>
2021-07-23 18:32:17 -0700

    Creating restore point

--- server.js
+++ server.js
@@ -1,5 +1,17 @@
 const express = require('express');
 const app = express();
-const data = require("./Develop")
+const data = require("./db/db.json")
 
 let PORT = 3001
+
+
+app.get('/api/notes', (req,res)=>{
+    res.json(data)
+})
+
+app.use(express.static('public'));
+
+
+app.listen(PORT, () =>
+  console.info(`Example app listening at http://localhost:${PORT} 🚀`)
+)
