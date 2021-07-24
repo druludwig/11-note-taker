d4e09d70de5e47b497adbc4f600c75fb0eb53ffa
Dru Ludwig <druludwig@yahoo.com>
2021-07-24 12:47:48 -0700

    Testing

--- server.js
+++ server.js
@@ -1,4 +1,5 @@
 const express = require('express');
+const fs = require('fs')
 const app = express();
 const path = require('path')
 const data = require("./db/db.json")
@@ -8,22 +9,23 @@ let PORT = 3001
 app.use(express.json());
 
 app.get('/notes', (req,res)=>{
-    res.sendFile(path.join(__dirname, 'public/notes.html'));
+    res.sendFile(path.join(__dirname, '/public/notes.html'));})
 
-})
+app.get('*', (req,res)=>{
+    res.sendFile(path.join(__dirname, '/public/index.html'));
+});
 
 app.get('/api/notes', (req,res)=>{
-    res.json(data)
+      res.json(data)
 })
 
 app.post('/api/notes', (req,res)=>{
-    data.push(req.body);
-    res.json(data) 
+
 })
 
 app.use(express.static('public'));
 
 
 app.listen(PORT, () =>
-  console.info(`Example app listening at http://localhost:${PORT} 🚀`)
+  console.info(`NoteTaker listening at http://localhost:${PORT} 🚀`)
 )
