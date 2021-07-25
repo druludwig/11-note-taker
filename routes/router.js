f068590129c1358bb67c0e4f03c5a16e41f72fbb
Dru Ludwig <druludwig@yahoo.com>
2021-07-25 16:04:43 -0700

    Cosmetic tuning

--- routes/router.js
+++ routes/router.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const app = express();
 const { readFromFile, readAndAppend } = require('../utils/file-handling');
+const { v4: uuidv4 } = require('uuid');
 
 app.get('/', (req, res) => { 
     readFromFile('./db/db.json').then((data) => res.json(JSON.parse(data)))
@@ -8,11 +9,31 @@ app.get('/', (req, res) => {
 
 app.get('/api/notes', (req,res)=>{
     readFromFile('./db/db.json').then((data) => res.json(JSON.parse(data)));
-
 });
 
-app.post('/api/notes', (req,res)=>{
+app.post('/', (req,res)=>{
+    const { title, text, feedback } = req.body;
 
+    // If all the required properties are present
+    if (title && text) {
+      // Variable for the object we will save
+      const newNote = {
+        title,
+        text,
+        id: uuidv4(),
+      };
+  
+      readAndAppend(newNote, './db/db.json');
+  
+      const response = {
+        status: 'success',
+        body: newNote,
+      };
+  
+      res.json(response);
+    } else {
+      res.json('Error in saving note');
+    }
 })
 
 
